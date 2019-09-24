a1e23fbc0bd28fcfd8c214db30ddd056cf77a0f4
Jesùs Garcìa <55461902+JesusGarcia143@users.noreply.github.com>
2019-09-23 23:03:30 -0500

    Add files via upload
    En este código se da una pequeña introducción de ¿que es? y¿como funciona?  al igual que la instalación de python .

---
+++ Script01.py
@@ -0,0 +1,13 @@
+Print('Hola a todos')
+
+#El comando print imprime un mensaje en la pantantalla o un dispositivo de salida.
+El mensaje puede ser una cadena de caracteres o cualquier objeto que sea combertida a una cadena de caracteres#
+#El comando imput le permite al usuario introducir informacion utilizando el 
+taclado. La variable donde se guarda dicha informacion es de tipo string o cadena de caracteres
+#El comando int convierte cualquier dato al tipo entero.
+#El comando float convierte cualquier dato al tipo decimal.
+
+edad= int(input)('Inserta tu edad: '))
+estatura= float(input('Inserta tu estatura:  '))
+print('Mi nombre es ', nombre, 'y naci en ' Naci en', 1998-edad, ' y mido ', estatura)
+print('Hola a todos TESH')
