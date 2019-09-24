aaa798266c8602654fda3e0ace0fb22cf4e234f5
Jesùs Garcìa <55461902+JesusGarcia143@users.noreply.github.com>
2019-09-23 23:08:09 -0500

    Add files via upload
    En este primer código se hace de forma practica el poder realizar mediante el programa de python un código que nos permita hacer un correo postal, de forma que se muestre  en el exterior de un sobre sin que  el usuario necesite meter datos adicionales

---
+++ Ejercicio1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+print('Alan Correa')
+print('San Mateo Nopala')
+print('Naucalpan, Edo de Mexico')
+print('11 de Septiembre del 2019')
+
+
+print('                                                          Jesus Garcia')
+print('                                                       Colonia El Hielo')
+print('                                               Municipio de Huixquilucan')
+print('                                                11 de Septiembre del 2019')
