37e369e2479d54d28a1751313b57231a4057b540
Jesùs Garcìa <55461902+JesusGarcia143@users.noreply.github.com>
2019-09-23 23:30:16 -0500

    Add files via upload
    En el ejercicio 3  se genera un código que le pide al usuario insertar el ancho y la longitud de una habitación para que el programa pueda calcular el área, la longitud y el ancho de la habitación usando medidas ya sean en metros o en pies, dependiendo de que medida se le facilite al usuario.

---
+++ Ejercicio3.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 15:01:59 2019
+
+@author: Jesús García
+"""
+largo= float(input('Ingrese medida de largo de la habitacion en metros '))
+ancho= float(input('Ingrese medida de ancho de la habitacion en metros '))
+print('El area de la habitacion es', largo*ancho,'m^2')
+
