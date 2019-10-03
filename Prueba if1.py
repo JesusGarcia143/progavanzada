046c28096f42165ea34fe3c7e198b08a2c905f8a
Jesùs Garcìa <55461902+JesusGarcia143@users.noreply.github.com>
2019-10-03 00:01:39 -0500

    Add files via upload
    En este pequeño ejercicio se pone en práctica la teoría sobre las Condicionales (if .... else).

---
+++ Prueba if1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 15:25:50 2019
+
+@author: Jesús García
+"""
+##Condicional if .... else
+
+password= input('Inserte password')
+
+if password =='Comunitesh':
+    print('Password correcto!!')
+else:
+    print('Te equivocaste!!')    
+
+
