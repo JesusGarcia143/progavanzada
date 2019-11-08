4f5ab72aded96c14856f17fe2355524c04b40351
Jesùs Garcìa <55461902+JesusGarcia143@users.noreply.github.com>
2019-11-08 02:01:57 -0600

    Add files via upload
    Escriba un programa que lea un mes y un día del usuario. Si el mes y el dia
    coincidir con uno de los días festivos enumerados anteriormente, entonces su programa debería mostrar el
    nombre de vacaciones De lo contrario, su programa debe indicar que el mes ingresado y
    día no corresponde a un día festivo de fecha fija.

---
+++ Ejercicio44.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  8 01:57:44 2019
+
+@author: Jesús García
+"""
+
+##Ejercicio: 44 "Fecha al nombre de vacaciones"
+
+mes =  entrada ( ' Mes: ' )
+day =  int ( input ( ' Day: ' ))
+
+vacaciones =  ' '
+
+si mes ==  ' enero '  y día ==  1 :
+ print ( ' día de año nuevo ' )
+elif mes ==  ' Julio '  y día ==  1 :
+ imprimir ( ' día de Canadá ' )
+elif mes ==  ' diciembre '  y día ==  25 :
+ imprimir ( ' día de Navidad ' )
+
+más :
+ print ( ' Mes y día mal ' )
