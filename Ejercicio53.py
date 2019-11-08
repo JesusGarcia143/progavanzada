7afb9cb1f26ad30fc3acf9ffddfb8aa619d3be73
Jesùs Garcìa <55461902+JesusGarcia143@users.noreply.github.com>
2019-11-08 02:21:34 -0600

    Add files via upload
    Escriba un programa que lea una calificación del usuario e indique si el desempeño
    mance era inaceptable, aceptable o meritorio. El monto del empleado
    
    aumento también debe ser reportado. Su programa debe mostrar un error apropiado
    mensaje si se ingresa una calificación no válida.

---
+++ Ejercicio53.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  8 02:20:00 2019
+
+@author: Jesús García
+"""
+
+##Ejercicio: 53 "Evaluación de empleados"
+
+valor =  flotante ( input ( ' Ingrese la calificación: ' ))
+
+rendimiento =  ' '
+
+si valor ==  0.0 :
+	rendimiento =  ' inaceptable '
+valor elif ==  0.4 :
+	rendimiento =  ' Aceptable '
+valor elif > =  0.6 :
+        rendimiento =  ' Meritorius '
+
+if performance ! =  ' ' :
+	print ( ' Eso no fue una calificación válida ' )
+más :
+	print ( ' Valor inválido (El valor es 0.0, 0.4 o mayor tahn 0.6) ' )
+si valor ==  0.0  o valor ==  0.4  o valor > = 0.6 :
+cantidad =  2400  * valor
+ 
+ print ( ' El increanse total es: ' , cantidad)
