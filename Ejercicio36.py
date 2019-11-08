612c3b36ab2bc74bc1fdb81ac56d250c515ecb89
Jesùs Garcìa <55461902+JesusGarcia143@users.noreply.github.com>
2019-11-08 01:15:46 -0600

    Add files via upload
    En este ejercicio se crea un programa que lee una letra del alfabeto del usuario. Si el usuario ingresa a, e, i, o u, entonces el programa muestra  un mensaje indicando que la letra ingresada es una vocal. Si el usuario ingresa y entonces el programa debería mostrar un mensaje que indica que a veces y es una vocal, y a veces y es una consonante. De lo contrario, su programa debería mostrar un mensaje que indica 
    ##que el letra es una consonante.

---
+++ Ejercicio36.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  8 00:57:25 2019
+
+@author: Jesús García
+"""
+
+##Ejercicio: 36 "Vocal o Consonante"
+
+##En este ejercicio creará un programa que lee una letra del alfabeto del
+##usuario. Si el usuario ingresa a, e, i, o u, entonces su programa debería mostrar un 
+##mensaje indicando que la letra ingresada es una vocal. Si el usuario ingresa y entonces
+##su programa debería mostrar un mensaje que indica que a veces y es una vocal, y a veces
+##y es una consonante. De lo contrario, su programa debería mostrar un mensaje que indica 
+##que el letra es una consonante.
+
+vocal = ( ' a ' , ' e ' , ' i ' , ' o ' , ' u ' )
+consonante = ( ' b ' , ' c ' , ' d ' , ' f ' , ' g ' , ' h ' , ' j ' , ' k ' , ' l ' , ' m ' , ' n ' , ' p ' , ' q ' , 'r ' ,' s ' , ' t ' , ' v ' , ' w ' , ' x ' , ' z ' )
+    if si es cierto :
+  letra =  entrada ( " Ingrese una letra: \ n " )
+  if letter.lower () ==  ' y ' :
+    print ( " Y es a veces una vocal y otras una consonante. \ n " )
+  letra elif en vocal:
+    print ( " % s es una vocal. \ n "  % (letter.upper ()))
+  letra elif en consonante:
+    print ( " % s es una consonante. \ n "  % (letter.upper ()))
+  más :
+    print ( " Entrada inválida. \ n " )
