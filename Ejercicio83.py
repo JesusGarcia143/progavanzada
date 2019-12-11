78dcd9c739c6e4f9e35850aa660d386b7ab9d1e2
Jesùs Garcìa <55461902+JesusGarcia143@users.noreply.github.com>
2019-12-10 23:18:31 -0600

    Add files via upload
    Un minorista en línea ofrece envío expreso para muchos de sus artículos a una tarifa de $ 10.95
    para el primer artículo y $ 2.95 por cada artículo posterior. Escribe una función que tome el
    número de elementos en el pedido como único parámetro. Devuelva los gastos de envío de
    el orden como resultado de la función. Incluya un programa principal que lea el número de
    artículos comprados al usuario y muestra los gastos de envío.

---
+++ Ejercicio83.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 10 23:16:38 2019
+
+@author: Jesús García
+"""
+##Ejercicio: 83 Calculadora de envío
+#Amazon provee envio express para muchos de sus productos a un coste de: $195.00 por el 
+##primer producto y de $29.50 para cada producto subsecuente. Escriba una funcion que 
+##tome el numero de porductos y los ordene como su unico argumento, regrese el costo de 
+##envio total como el resultado de la funcion. Incluya un programa principal que lea el 
+##numero de productos comprados por el usuario y que desplegue el costo total de envio.
+
+def costo(articulos):
+    total = 195.00 + (articulos-1)*29.50
+    if d == 1:
+        print('195.00: ')
+    elif d > 1:
+        print('El costo del envio es: ' , total)
+    return total
+    
+d = float(input('Inserte numero de articulos:'))
+final = costo(d)
