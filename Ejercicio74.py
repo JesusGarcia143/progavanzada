887c625e9618de1a2343938f3215948255edfa70
Jesùs Garcìa <55461902+JesusGarcia143@users.noreply.github.com>
2019-12-10 23:00:25 -0600

    Add files via upload
    En este ejercicio creará un programa que muestra una tabla de multiplicación que
    muestra los productos de todas las combinaciones de enteros desde 1 por 1 hasta
    10 veces 10. Su tabla de multiplicar debe incluir una fila de etiquetas en la parte superior
    contiene los números del 1 al 10. También debe incluir etiquetas a la izquierda
    lado que consiste en los números del 1 al 10. La salida esperada del programa
    se muestra a continuación:

---
+++ Ejercicio74.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 10 22:58:34 2019
+
+@author: Jesús García
+"""
+##Ejercicio: 74  Tabla de multiplicación
+n = int(input('Enter a number: '))
+
+# use for loop to iterate 10 times
+for i in range(1,11):
+   print(n,'x',i,'=',n*i)
