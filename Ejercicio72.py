931dad92afd23762df2e14b5c6910e2c0e1b1841
Jesùs Garcìa <55461902+JesusGarcia143@users.noreply.github.com>
2019-12-10 22:56:01 -0600

    Add files via upload
    Existen numerosas frases que son palíndromos cuando se ignora el espacio. Ejemplos
    incluyen "ir perro", "huir a mí elfo remoto" y "algunos hombres interpretan nueve notas",
    Entre muchos otros. Extienda su solución al Ejercicio 72 para que ignore el espaciado
    mientras determina si una cuerda es o no un palíndromo. Para un desafío adicional,
    extienda su solución para que también ignore los signos de puntuación y trate las mayúsculas
    y letras minúsculas como equivalentes.

---
+++ Ejercicio72.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 10 22:54:00 2019
+
+@author: Jesús García
+"""
+##Ejercicio: 72   Es una cuerda un palíndromo
+##escribir un programa que determine si una palabra insertada por el usuario es palodromo o no lo es
+##un palondromo es una palabra que se lee igual de izquierda a derecha que de derecha a izquierda, por ejemplo
+##radar, rotor, rallar, sometemos.
+
+class cadenas:
+    def __init__ (self, cade1):
+        self.cade1=cade1
+ 
+    def Palidromo(self):
+        cade1 = self.cade1
+        c,i,nom,cade,x = 0,0,'','',''
+        i = len(cade1)
+        nom = cade1.lower()
+        
+        while i != c:
+            for x in nom:
+                cade = x + cade
+                c=c+1
+            if nom==cade:
+                #print (cade1, 'Palindromo')
+                return str(cade1 + 'Palindromo')
+            else:
+                #print (cade1, 'Error de Palindromo')
+                return str(cade1 + 'Error de Palindromo')
+ 
+cade1 = input('Escribe una palabra')
+op1=cadenas(cade1)
+ 
+print(op1.Palidromo())
